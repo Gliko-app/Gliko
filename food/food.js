eb089066bb08f3cea90da4f0c2769786a58bc4c6
JocaNBG <ddoslo@gmail.com>
2025-09-29 23:58:25 +0200

    Delete food/food.js

--- food/food.js
+++
@@ -1,9 +0,0 @@
-// Carousel - inicijalizacija i smanjenje slika
-(function initCarousel() {
-  const carouselItems = document.querySelectorAll('.carousel-item img');
-  carouselItems.forEach(item => {
-    // Smanjujemo dimenzije slika
-    item.style.maxWidth = '100%';
-    item.style.height = 'auto';
-  });
-})();
